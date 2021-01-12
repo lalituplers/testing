7eced931574b2f96418b66b4d653d21d2349d058
Lalitkumar Pendhare <UP0163@uplers.local>
2021-01-13 00:31:00 +0530

    add html code

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+<a href="link1.php">link1</a>
+<a href="link2.php">link2</a>
+</body>
+</html>
